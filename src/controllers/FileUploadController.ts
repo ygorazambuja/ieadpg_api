cb85a450db7eef1c9fdd2ebc55be51ff0395b4ee
Ygor Azambuja <ygorazambuja@gmail.com>
2022-02-24 19:50:57 -0400

    fix: add error treatment

--- src/controllers/FileUploadController.ts
+++ src/controllers/FileUploadController.ts
@@ -5,10 +5,16 @@ export class FileUploadController {
   async handle(request: Request, response: Response) {
     const { file } = request;
 
-    const person = await parseFile(file.path);
+    try {
+      const person = await parseFile(file.path);
 
-    return response.json({
-      person,
-    });
+      return response.json({
+        person,
+      });
+    } catch (error) {
+      return response.json({
+        error,
+      });
+    }
   }
 }

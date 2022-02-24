cba9d6f77a42067ab2d93016718df175d492c1eb
Ygor Azambuja <ygorazambuja@gmail.com>
2022-02-24 19:40:18 -0400

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import router from "./routes";
 
@@ -7,6 +8,7 @@ const app = express();
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
